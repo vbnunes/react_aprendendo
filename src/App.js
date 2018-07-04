651393f5d676ea6a6e21ec4473732b4afaa6024d
Vinicius Nunes <vnsbnunes@gmail.com>
2018-07-04 15:35:05 -0300

    Utilizando a API e componentizando os inputs

--- src/App.js
+++ src/App.js
@@ -1,70 +1,125 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {lista : [], nome:'', email:'', senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  } 
+
+  componentWillMount(){
+    console.log("willMount");
+    $.ajax({
+        url:"http://localhost:8080/api/autores",
+        dataType: 'json',
+        success:function(resposta){
+          this.setState({lista:resposta});
+          }.bind(this)
+    }
+  );
+  }
+
+  enviaForm(evento){
+    evento.preventDefault();
+    $.ajax({
+      url: 'http://localhost:8080/api/autores',
+      contentType: 'application/json',
+      dataType:'json',
+      type: 'post',
+      data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
+      success: function(resposta){
+        this.setState({lista:resposta});
+        console.log("dados enviads com sucesso");
+      }.bind(this),
+      error: function(resposta){
+        console.log("erro");
+      }
+    });
+  }
+
+  setNome(evento){
+    this.setState({nome:evento.target.value});
+  }
+  setEmail(evento){
+    this.setState({email:evento.target.value})
+  }
+  setSenha(evento){
+    this.setState({senha:evento.target.value})
+  }
+
   render() {
     return (
     <div id="layout">
-        <a href="#menu" id="menuLink" class="menu-link">
+        <a href="#menu" id="menuLink" className="menu-link">
             <span></span>
         </a>
 
         <div id="menu">
-            <div class="pure-menu">
-                <a class="pure-menu-heading" href="#">Company</a>
+            <div className="pure-menu">
+                <a className="pure-menu-heading" href="#">Company</a>
 
-                <ul class="pure-menu-list">
-                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
-                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">About</a></li>
+                <ul className="pure-menu-list">
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
 
-                    <li class="pure-menu-item menu-item-divided pure-menu-selected">
-                        <a href="#" class="pure-menu-link">Services</a>
+                    <li className="pure-menu-item menu-item-divided pure-menu-selected">
+                        <a href="#" className="pure-menu-link">Services</a>
                     </li>
 
-                    <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
                 </ul>
             </div>
         </div>
 
         <div id="main">
-            <div class="header">
-                <h1>Page Title</h1>
-                <h2>A subtitle for your page goes here</h2>
+            <div className="header">
+              <h1>Cadastro de Autores</h1>
             </div>
+            <div className="content" id="content">
+              <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
+                  <InputCustomizado id='nome' type='text' name='nome' value={this.state.nome} onChange={this.setNome} label='Nome'/>
+                  <InputCustomizado id='email' type='email' name='email' value={this.state.email} onChange={this.setEmail} label='Email'/>
+                  <InputCustomizado id='senha' type='password' name='senha' value={this.state.senha} onChange={this.setSenha} label='Senha'/>
 
-            <div class="content">
-                <h2 class="content-subhead">How to use this layout</h2>
-                <p>
-                    To use this layout, you can just copy paste the HTML, along with the CSS in <a href="/css/layouts/side-menu.css" alt="Side Menu CSS">side-menu.css</a>, and the JavaScript in <a href="/js/ui.js">ui.js</a>. The JS file uses vanilla JavaScript to simply toggle an <code>active</code> class that makes the menu responsive.
-                </p>
+                  <div className="pure-control-group">                                  
+                    <label></label> 
+                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+                  </div>
+                </form>             
 
-                <h2 class="content-subhead">Now Let's Speak Some Latin</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-
-                <div class="pure-g">
-                    <div class="pure-u-1-4">
-                        <img class="pure-img-responsive" src="http://farm3.staticflickr.com/2875/9069037713_1752f5daeb.jpg" alt="Peyto Lake"/>
-                    </div>
-                    <div class="pure-u-1-4">
-                        <img class="pure-img-responsive" src="http://farm3.staticflickr.com/2813/9069585985_80da8db54f.jpg" alt="Train"/>
-                    </div>
-                    <div class="pure-u-1-4">
-                        <img class="pure-img-responsive" src="http://farm6.staticflickr.com/5456/9121446012_c1640e42d0.jpg" alt="T-Shirt Store"/>
-                    </div>
-                    <div class="pure-u-1-4">
-                        <img class="pure-img-responsive" src="http://farm8.staticflickr.com/7357/9086701425_fda3024927.jpg" alt="Mountain"/>
-                    </div>
-                </div>
-
-                <h2 class="content-subhead">Try Resizing your Browser</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+              </div>  
+              <div>            
+                <table className="pure-table">
+                  <thead>
+                    <tr>
+                      <th>Nome</th>
+                      <th>email</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      this.state.lista.map(function(autor){
+                        return(
+                          <tr key={autor.id}>
+                            <td>{autor.nome}</td>
+                            <td>{autor.email}</td>
+                          </tr>
+                        );                        
+                      })
+                    }
+                  </tbody>
+                </table> 
+              </div>             
             </div>
-        </div>
+          </div>         
     </div>
 
     );
